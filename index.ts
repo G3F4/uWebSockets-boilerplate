11b4f573c0c93efdc851bbc1d09ddc3117f23064
Kamil Grabek <kamil.grabek@allegro.pl>
2019-07-08 23:40:07 +0200

    moves @types/node to dependencies.

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import { App } from 'uWebSockets.js';
 
+const PORT = parseInt(process.env.PORT, 10) || 3001;
+
 // http://localhost:3001/test/testParametruPierwszego/test/testParametruDrugiego/?parampierwszy=test1&paramdrugi=test2
 const server = App().get('/test/:firstParam/test/:secondParam', (res, req) => {
-  // prints User-Agent header
+  // print User-Agent header
   process.stdout.write(req.getHeader('user-agent'));
   process.stdout.write(`${req.getMethod()}\n`);
   process.stdout.write(`${req.getQuery()}\n`);
@@ -10,14 +12,14 @@ const server = App().get('/test/:firstParam/test/:secondParam', (res, req) => {
   process.stdout.write(`${req.getParameter(0)}\n`);
   process.stdout.write(`${req.getParameter(1)}\n`);
 
-  // sets Content-Type header
+  // set Content-Type header
   res.writeHeader('Content-Type', 'text/html');
 
   // write simple html doc
   res.write('<html>');
   res.write('<body>');
   res.write('<div>');
-  res.write('hello world!');
+  res.write('hello from uWebSockets.js!');
   res.write('</div>');
   res.write('</body>');
   res.write('</html>');
@@ -27,8 +29,9 @@ const server = App().get('/test/:firstParam/test/:secondParam', (res, req) => {
 
 });
 
-server.listen(3001, listenSocket => {
-
+// start server by setting PORT to listening
+server.listen(PORT, listenSocket => {
+  // if callback function argument is defined, then server is up and running
   if (listenSocket) {
     process.stdout.write('\x1b[33m');
     process.stdout.write('Listening to port 3001\n');
